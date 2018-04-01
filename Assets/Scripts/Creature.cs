2afe73e918640b6e7375f40f292c94a10da855b9
YANG WANG <yang.wang.dec@gmail.com>
2018-04-01 19:44:55 -0400

    behavior and event stubs

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+abstract public class Creature : MonoBehaviour
+{
+
+    NavMeshAgent nmAgent;
+
+    private void Awake()
+    {
+        nmAgent = GetComponent<NavMeshAgent>();
+    }
+
+    //Behaviours
+    protected void SetTarget(Vector3 target)
+    {
+        nmAgent.SetDestination(target);
+    }
+
+    protected void RotateSelf(Vector3 angle)
+    {
+        transform.Rotate(angle);
+    }
+
+    protected void SetScale(Vector3 mul)
+    {
+
+    }
+
+    protected void Speak(string content)
+    {
+
+    }
+
+    protected void Spark(string ParticleType)
+    {
+
+    }
+
+    protected void PlaySound(string soundName)
+    {
+
+    }
+
+    protected void PlayAnimation(string animationName)
+    {
+
+    }
+
+
+    //Events
+    void OnMeetSomeone(GameObject other)
+    {
+
+    }
+
+    void OnLeaveSomeone(GameObject other)
+    {
+
+    }
+
+    void OnSunrise()
+    {
+
+    }
+
+    void OnSunset()
+    {
+
+
+    }
+
+
+
+
+    //others
+
+    public static Vector3 RandomVec3(float a, float b)
+    {
+        return new Vector3(Random.Range(a, b), Random.Range(a, b), Random.Range(a, b));
+    }
+
+
+
+}
---
+++ Assets/Scripts/KiuYW.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class KiuYW : Creature {
+
+
+    private void Start()
+    {
+    //    SetTarget(RandomVec3(-100,100));
+        transform.Rotate(RandomVec3(-90,90));
+    }
+
+    private void Update()
+    {
+        //for testing
+        if(Input.GetMouseButton(0))
+        {
+            //SetTarget(RandomVec3(-100, 100));
+            transform.Rotate(new Vector3(0,1,0));
+        }
+
+
+        if (Input.GetMouseButton(1))
+        {
+            SetTarget(RandomVec3(-100, 100));
+            //transform.Rotate(new Vector3(0, 1, 0));
+        }
+
+    }
+
+
+
+
+}
