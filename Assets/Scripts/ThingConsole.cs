87f085ed96fb080f7e26898a83de34af8f8bfd0b
Yang Wang <yang.wang.dec@gmail.com>
2018-10-14 19:29:15 -0400

    update helper

---
+++ Assets/Scripts/ThingConsole.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThingConsole : MonoBehaviour {
+
+    static float verticalNormalizedPosition = 0;
+    static Text consoleText;
+    static ScrollRect scrollRect;
+    static int maxLength = 3000;
+
+    static StringBuilder stringBuilder;
+
+    private void Awake () {
+        stringBuilder = new StringBuilder ();
+        consoleText = GetComponentInChildren<Text> ();
+        scrollRect = GetComponentInChildren<ScrollRect> ();
+    }
+
+
+    public static void Log (string content) {
+
+        stringBuilder.Append (content);
+        consoleText.text = stringBuilder.ToString ();
+
+        if (stringBuilder.Length > maxLength) {
+            stringBuilder.Remove (0, stringBuilder.Length - maxLength);
+        }
+
+        scrollRect.verticalNormalizedPosition = verticalNormalizedPosition;
+
+    }
+}
--- Assets/ThingCreationHelper/Editor/ThingCreationHelper.cs
+++ Assets/ThingCreationHelper/Editor/ThingCreationHelper.cs
@@ -1,83 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
 using System.IO;
+using UnityEditor;
+using UnityEngine;
 
+public class ThingCreationHelper : EditorWindow {
 
+    string thingName = "";
+    string creatorName = "";
+    Object prefab;
 
-    public class ThingCreationHelper : EditorWindow
-    {
+    public Object speechBubblePrefab;
+    public Object particleSystemPrefab;
 
+    [MenuItem ("(｡・`ω´・) ThingThingThing/Helper")]
+    static void CreateNewThing () {
+        EditorWindow.GetWindow (typeof (ThingCreationHelper));
+    }
 
-        string thingName = "";
-        string creatorName = "";
-        Object prefab;
+    void OnGUI () {
+        GUILayout.Label ("Prepare my directory", EditorStyles.boldLabel);
+        thingName = EditorGUILayout.TextField ("Name of the THING", thingName);
+        creatorName = EditorGUILayout.TextField ("Name of the Creator", creatorName);
 
-        public Object speechBubblePrefab;
-        public Object particleSystemPrefab;
-
-        [MenuItem("(｡・`ω´・) Thing-Thing-Thing/Create A New Thing")]
-        static void CreateNewThing()
-        {
-            EditorWindow.GetWindow(typeof(ThingCreationHelper));
+        if (GUILayout.Button ("Generate")) {
+            CreateMyDir ();
+            CopyWorldScene (thingName, creatorName);
+            CreateMaterial (thingName, creatorName);
+            Debug.Log (thingName + "," + creatorName);
         }
 
+        /* 
+                    GUILayout.Label("Prepare my THING", EditorStyles.boldLabel);
+                    prefab = EditorGUILayout.ObjectField(prefab, typeof(GameObject), true);
 
-        void OnGUI()
-        {
-            GUILayout.Label("Prepare my directory", EditorStyles.boldLabel);
-            thingName = EditorGUILayout.TextField("Name of the THING", thingName);
-            creatorName = EditorGUILayout.TextField("Name of the Creator", creatorName);
-
-            if (GUILayout.Button("Generate"))
-            {
-                CreateMyDir();
-                CopyWorldScene(thingName, creatorName);
-                Debug.Log(thingName + "," + creatorName);
-            }
-
-            GUILayout.Label("Prepare my THING", EditorStyles.boldLabel);
-            prefab = EditorGUILayout.ObjectField(prefab, typeof(GameObject), true);
-
-            if (GUILayout.Button("Initialize my THING"))
-            {
-                AttachSpeechBubble(prefab);
-                AttachParticleSystem(prefab);
-            }
-
-
-        }
-
-        void CreateMyDir()
-        {
-            string dirPath = Path.Combine(Application.dataPath, "CREATORS/" + creatorName);
-            Debug.Log("creating folder at path: " + dirPath);
-            Directory.CreateDirectory(dirPath);
-        }
-
-        void CopyWorldScene(string thingName, string creatorName)
-        {
-            string original = Application.dataPath + "/Scenes/TTTWorldScene.unity";
-            string desk = Application.dataPath + "/CREATORS/" + creatorName + "/TTTWorldScene_" + creatorName + ".unity";
-            FileUtil.CopyFileOrDirectory(original, desk);
-        }
-
-        void AttachSpeechBubble(Object obj)
-        {
-            GameObject sb = (GameObject)PrefabUtility.InstantiatePrefab(Resources.Load("Chat Balloon"));
-            GameObject gobj = (GameObject)obj;
-            sb.transform.parent = gobj.transform;
-            sb.transform.localPosition = Vector3.zero;
-        }
-
-        void AttachParticleSystem(Object obj)
-        {
-            GameObject sb = (GameObject)PrefabUtility.InstantiatePrefab(Resources.Load("Particle Explode"));
-            GameObject gobj = (GameObject)obj;
-            sb.transform.parent = gobj.transform;
-            sb.transform.localPosition = Vector3.zero;
-        }
+                    if (GUILayout.Button("Initialize my THING"))
+                    {
+                        AttachSpeechBubble(prefab);
+                        AttachParticleSystem(prefab);
+                    }
+        */
 
     }
 
+    void CreateMyDir () {
+        string dirPath = Path.Combine (Application.dataPath, "CREATORS/" + creatorName);
+        Debug.Log ("creating folder at path: " + dirPath);
+        Directory.CreateDirectory (dirPath);
+    }
+
+    void CopyWorldScene (string thingName, string creatorName) {
+        string original = Application.dataPath + "/Scenes/TTTWorldScene.unity";
+        string dest = Application.dataPath + "/CREATORS/" + creatorName + "/TTTWorldScene_" + creatorName + ".unity";
+        FileUtil.CopyFileOrDirectory (original, dest);
+    }
+
+    void CreateMaterial (string thingName, string creatorName) {
+        string destPath = "Assets/CREATORS/" + creatorName + "/Material_" + creatorName + ".mat";
+        AssetDatabase.CreateAsset (new Material (Shader.Find ("ThingThingThing/Main")), destPath);
+    }
+
+    void AttachSpeechBubble (Object obj) {
+        GameObject sb = (GameObject) PrefabUtility.InstantiatePrefab (Resources.Load ("Chat Balloon"));
+        GameObject gobj = (GameObject) obj;
+        sb.transform.parent = gobj.transform;
+        sb.transform.localPosition = Vector3.zero;
+    }
+
+    void AttachParticleSystem (Object obj) {
+        GameObject sb = (GameObject) PrefabUtility.InstantiatePrefab (Resources.Load ("Particle Explode"));
+        GameObject gobj = (GameObject) obj;
+        sb.transform.parent = gobj.transform;
+        sb.transform.localPosition = Vector3.zero;
+    }
+
+}
