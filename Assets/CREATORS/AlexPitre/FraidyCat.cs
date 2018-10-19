99e9e252260b398234956ad745c0c0a2b3b5dd59
AlexP <ay.a.pea@gmail.com>
2018-10-18 20:47:59 -0400

    AlexPitre as FraidyCat

---
+++ Assets/CREATORS/AlexPitre/FraidyCat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FraidyCat : Thing {
+	 
+	AudioSource FCsound;
+
+	protected override void OnMeetingSomeone(GameObject other){
+		
+		ChangeColor (Color.cyan);
+		FCsound = GetComponent<AudioSource> ();
+		FCsound.Play (0);
+
+	}
+
+
+
+	protected override void OnNeighborSpeaking(){
+		ResetColor ();
+	}
+}
