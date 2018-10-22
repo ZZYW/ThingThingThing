ddb03a09e7c234312c750304f646416b72b7eafa
Yang Wang <yang.wang.dec@gmail.com>
2018-10-22 18:33:54 -0400

    use CharacterController to move to gain collision detection

--- Assets/Scripts/ControlableCameraController.cs
+++ Assets/Scripts/ControlableCameraController.cs
@@ -4,53 +4,56 @@ using UnityEngine;
 
 public class ControlableCameraController : MonoBehaviour {
 
+    public enum ControlMode {
+        JOYSTICKS,
+        KEYBOARD_MOUSE
+    }
+
+    public ControlMode controlMode;
+
     [Range (0, 50)]
     public float speedFactor = 0.1f;
 
-    [Range(0, 100)]
+    [Range (0, 100)]
     public float rotateSpeedFactor = 80;
 
-    [Range (1, 10)]
-    public float jumpSpeed = 8.0f;
-    [Range (1, 20)]
-    public float gravity = 20.0f;
+    Vector2 leftAxis;
+    Vector2 rightAxis;
 
-    
-    private Vector2 leftAxis;
-    private Vector2 rightAxis;
-    
-
-    
+    // Rigidbody rb;
+    CharacterController controller;
 
     // Use this for initialization
     void Start () {
-        // characterController = GetComponent<CharacterController>();
+        // rb = GetComponent<Rigidbody> ();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update () {
 
-        leftAxis.x = Input.GetAxis("JoyStickLeftX");
-        leftAxis.y = Input.GetAxis("JoyStickLeftY");
-        rightAxis.x = Input.GetAxis("JoyStickRightX");
-        rightAxis.y = Input.GetAxis("JoyStickRightY");
-
-
-        // Debug.Log("left axis: " + leftAxis + "    right axis:  " + rightAxis);
-        
+        if (controlMode == ControlMode.JOYSTICKS) {
+            leftAxis.x = Input.GetAxis ("JoyStickLeftX");
+            leftAxis.y = Input.GetAxis ("JoyStickLeftY");
+            rightAxis.x = Input.GetAxis ("JoyStickRightX");
+            rightAxis.y = Input.GetAxis ("JoyStickRightY");
+        } else if (controlMode == ControlMode.KEYBOARD_MOUSE) {
+            leftAxis.x = Input.GetAxis ("Horizontal");
+            leftAxis.y = Input.GetAxis ("Vertical");
+            rightAxis.x = Input.GetAxis ("Mouse X");
+            rightAxis.y = Input.GetAxis ("Mouse Y");
+        }
 
         leftAxis *= speedFactor * Time.deltaTime;
         rightAxis *= rotateSpeedFactor * Time.deltaTime;
 
+        Vector3 translation = new Vector3 (leftAxis.x, 0, leftAxis.y);
+        translation = transform.TransformVector (translation);
+        controller.Move(translation);
         
-
-        transform.Translate(new Vector3(leftAxis.x, 0, leftAxis.y), Space.Self);
-        transform.Rotate(Vector3.up, rightAxis.x);
-        transform.Rotate(Vector3.left, rightAxis.y);
-
-
-
-        // characterController.Move(motion * Time.deltaTime);
+        // rb.MovePosition (transform.position + translation);
+        transform.Rotate (Vector3.up, rightAxis.x);
+        transform.Rotate (Vector3.left, rightAxis.y);
 
     }
 
