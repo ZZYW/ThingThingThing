f237e303e1275ec9c2aad6361da12d93f73ccf2e
Yang Wang <yang.wang.dec@gmail.com>
2018-10-22 22:31:56 -0400

    added untracked materaisl

---
+++ Assets/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	public List<Renderer> renderers = new List<Renderer> ();
+
+	// Use this for initialization
+	void Start () {
+
+		Transform[] gos = FindObjectsOfType<Transform> ();
+		foreach (var GO in gos) {
+			if (GO.gameObject.GetComponent<Renderer> ()) {
+				renderers.Add (GO.gameObject.GetComponent<Renderer> ());
+
+			}
+		}
+
+		foreach (var rend in renderers) {
+			var mats = rend.materials;
+			foreach (var mat in mats) {
+				mat.shader = Shader.Find ("Unlit/Color");
+			}
+
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
