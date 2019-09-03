85cda96053fb14ffc187ded348b38c1b5df9f9d5
zz q <zhenzhenqi@gmail.com>
2019-09-03 13:40:04 -0400

    metalic materials

---
+++ Assets/Scripts/DevTool.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevTool : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            string filename =  System.DateTime.Now.ToString();
+
+            filename = filename.Replace("/", "-");
+            filename = filename.Replace(" ", "_");
+            filename = filename.Replace(":", "-");
+            ScreenCapture.CaptureScreenshot(filename + ".png" , 4);
+        }
+        
+    }
+}
