a22c1b36f16828db94c4f74973f393ee9525b567
Yang Wang <yang.wang.dec@gmail.com>
2019-10-31 13:56:56 -0400

    auto adjust camera x position to seamless stitch

---
+++ Assets/TwoScreenAdjust.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoScreenAdjust : MonoBehaviour
+{
+
+    public Camera leftCam;
+    public Camera rightCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //unify size
+        float size = leftCam.orthographicSize;
+        rightCam.orthographicSize = size;
+        //calculate offset
+        float offsetValue = leftCam.aspect * leftCam.orthographicSize;
+
+        var leftCamLoPos = leftCam.transform.localPosition;
+        leftCamLoPos.x = -offsetValue;
+        leftCam.transform.localPosition = leftCamLoPos;
+
+        var rightCamLoPos = rightCam.transform.localPosition;
+        rightCamLoPos.x = offsetValue;
+        rightCam.transform.localPosition = rightCamLoPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
