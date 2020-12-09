582637e0832e87fd1ab86c3d27e44a662ad0e89c
Yang Wang <yang.wang.dec@gmail.com>
2020-12-08 22:16:24 -0500

    screen coord pos thing

---
+++ Assets/Scripts/InScreenCoord.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InScreenCoord : MonoBehaviour
+{
+    public LineRenderer r, g, b;
+    public float length;
+    LineRenderer[] lines;
+    public float width = 1;
+    public Vector3 screenPositon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lines = new LineRenderer[] { r, g, b };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Camera.main.ViewportToScreenPoint(screenPositon);
+        transform.localRotation = Camera.main.transform.rotation;
+
+
+        r.SetPositions(new Vector3[] { Vector3.zero, Vector3.right * length });
+        g.SetPositions(new Vector3[] { Vector3.zero, Vector3.up * length });
+        b.SetPositions(new Vector3[] { Vector3.zero, Vector3.forward * length });
+
+        foreach (var line in lines)
+        {
+            line.startWidth = width;
+            line.endWidth = width;
+        }
+    }
+}
--- Assets/Scripts/Thing.cs
+++ Assets/Scripts/Thing.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
 
-[RequireComponent (typeof (BoxCollider))]
-[RequireComponent (typeof (Rigidbody))]
-[RequireComponent (typeof (AudioSource))]
-[RequireComponent (typeof (ThingMotor))]
-public class Thing : MonoBehaviour {
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(ThingMotor))]
+public class Thing : MonoBehaviour
+{
 
-    public class Settings {
+    public class Settings
+    {
         public int cameraOffset;
         public float acceleration;
         public float drag;
@@ -23,7 +25,8 @@ public class Thing : MonoBehaviour {
         public Color myCubeColor;
         public int neighborDetectorRadius;
 
-        public Settings () {
+        public Settings()
+        {
             cameraOffset = 15;
             acceleration = 4;
             drag = 1.8f;
@@ -73,63 +76,69 @@ public class Thing : MonoBehaviour {
     Color originalColor;
     StringBuilder stringBuilder;
 
-    [Header ("Your Main Material For Color Changing")]
+    [Header("Your Main Material For Color Changing")]
     [SerializeField] Material mMat;
 
     public int DesiredFollowDistance { get { return settings.cameraOffset; } }
 
-    private void OnEnable () {
+    private void OnEnable()
+    {
         TTTEventsManager.OnSomeoneSpeaking += OnSomeoneSpeaking;
         TTTEventsManager.OnSomeoneSparking += OnSomeoneSparking;
         TOD_Data.OnSunset += OnSunset;
         TOD_Data.OnSunrise += OnSunrise;
     }
 
-    private void OnDisable () {
+    private void OnDisable()
+    {
         TTTEventsManager.OnSomeoneSpeaking -= OnSomeoneSpeaking;
         TTTEventsManager.OnSomeoneSparking -= OnSomeoneSparking;
         TOD_Data.OnSunset -= OnSunset;
         TOD_Data.OnSunrise -= OnSunrise;
-        CancelInvoke ();
+        CancelInvoke();
     }
 
-    private void Awake () {
-        speakCD = new Cooldown (Random.Range (5f, 10f));
-        playSoundCD = new Cooldown (1);
+    private void Awake()
+    {
+        speakCD = new Cooldown(Random.Range(5f, 10f));
+        playSoundCD = new Cooldown(1);
 
         MyName = gameObject.name;
-        settings = new Settings ();
-        stringBuilder = new StringBuilder ();
+        settings = new Settings();
+        stringBuilder = new StringBuilder();
         gameObject.tag = thingTag;
         gameObject.layer = 14;
-        neighborList = new List<GameObject> ();
-        TTTAwake ();
+        neighborList = new List<GameObject>();
+        TTTAwake();
     }
 
-    private void Start () {
+    private void Start()
+    {
         //neighbor detector
         // neighborDetector = GetComponent<SphereCollider> ();
         // neighborDetector.isTrigger = true;
         //no longer use sphere collider as neighbor detector
-        if (GetComponent<SphereCollider> ()) {
-            Destroy (GetComponent<SphereCollider> ());
+        if (GetComponent<SphereCollider>())
+        {
+            Destroy(GetComponent<SphereCollider>());
         }
 
         //motor
-        motor = GetComponent<ThingMotor> ();
-        motor.SetAccel (settings.acceleration * 4);
+        motor = GetComponent<ThingMotor>();
+        motor.SetAccel(settings.acceleration * 4);
         motor.rb.drag = settings.drag;
         motor.rb.mass = settings.mass;
-        motor.FacingTarget (settings.alwaysFacingTarget);
+        motor.FacingTarget(settings.alwaysFacingTarget);
 
         //Instantiating Particle Object
-        explodePS = GetComponentInChildren<ParticleSystem> ();
-        if (explodePS == null) {
-            ThingConsole.LogWarning (FormatString ("{0} doesn't have a particle system?!", MyName));
+        explodePS = GetComponentInChildren<ParticleSystem>();
+        if (explodePS == null)
+        {
+            ThingConsole.LogWarning(FormatString("{0} doesn't have a particle system?!", MyName));
         }
 
         //Sound
-        audioSource = gameObject.GetComponent<AudioSource> ();
+        audioSource = gameObject.GetComponent<AudioSource>();
         audioSource.playOnAwake = false;
         audioSource.loop = false;
         audioSource.bypassListenerEffects = false;
@@ -137,124 +146,156 @@ public class Thing : MonoBehaviour {
         audioSource.maxDistance = 35;
         audioSource.dopplerLevel = 5;
 
-        TTTStart ();
+        TTTStart();
 
-        speakCD.GoCooldown ();
+        speakCD.GoCooldown();
 
     }
-    private void Update () {
+    private void Update()
+    {
         //check cooldown
-        speakCD.Check ();
-        playSoundCD.Check ();
+        speakCD.Check();
+        playSoundCD.Check();
 
         //check position
-        if (Vector3.Distance (transform.position, transform.parent.position) > 200) {
-            ResetPosition ();
+        if (Vector3.Distance(transform.position, transform.parent.position) > 200)
+        {
+            ResetPosition();
         }
 
+
+
         //check neighbors
-        foreach (GameObject t in ThingManager.main.AllThings) {
-            float dist = Vector3.Distance (transform.position, t.transform.position);
-            if (dist < settings.neighborDetectorRadius) {
-                if (!neighborList.Contains (t)) {
-                    neighborList.Add (t);
-                    OnMeetingSomeone (t);
+        foreach (GameObject t in ThingManager.main.AllThings)
+        {
+            float dist = Vector3.Distance(transform.position, t.transform.position);
+            if (dist < settings.neighborDetectorRadius)
+            {
+                if (!neighborList.Contains(t))
+                {
+                    neighborList.Add(t);
+                    OnMeetingSomeone(t);
                 }
-            } else {
-                if (neighborList.Contains (t)) {
-                    neighborList.Remove (t);
-                    OnLeavingSomeone (t);
+            }
+            else
+            {
+                if (neighborList.Contains(t))
+                {
+                    neighborList.Remove(t);
+                    OnLeavingSomeone(t);
                 }
             }
         }
 
-        TTTUpdate ();
-    }
+        TTTUpdate();    
 
-    private void OnSomeoneSpeaking (GameObject who) {
-        if (neighborList.Contains (who)) {
-            OnNeighborSpeaking ();
+    }
+    
+
+    private void OnSomeoneSpeaking(GameObject who)
+    {
+        if (neighborList.Contains(who))
+        {
+            OnNeighborSpeaking();
         }
     }
 
-    private void OnSomeoneSparking (GameObject who) {
-        if (neighborList.Contains (who)) {
-            OnNeigborSparkingParticles ();
+    private void OnSomeoneSparking(GameObject who)
+    {
+        if (neighborList.Contains(who))
+        {
+            OnNeigborSparkingParticles();
         }
     }
 
     //use string builder to concat string to avoid memory leak
-    private string FormatString (string format, params object[] args) {
+    private string FormatString(string format, params object[] args)
+    {
         stringBuilder.Length = 0;
-        stringBuilder.AppendFormat (format, args);
-        return stringBuilder.ToString ();
+        stringBuilder.AppendFormat(format, args);
+        return stringBuilder.ToString();
     }
 
-    private void RescueFromWater () {
-        if (InWater) {
-            ResetPosition ();
-            ThingConsole.LogWarning (FormatString ("{0} is rescued from water", MyName));
+    private void RescueFromWater()
+    {
+        if (InWater)
+        {
+            ResetPosition();
+            ThingConsole.LogWarning(FormatString("{0} is rescued from water", MyName));
         }
     }
 
-    public void OnWaterEnter () {
+    public void OnWaterEnter()
+    {
         InWater = true;
-        Invoke ("RescueFromWater", 60f);
-        OnTouchWater ();
+        Invoke("RescueFromWater", 60f);
+        OnTouchWater();
     }
 
-    public void OnWaterExit () {
+    public void OnWaterExit()
+    {
         InWater = false;
-        CancelInvoke ("RescueFromWater");
-        OnLeaveWater ();
-        ThingConsole.Log (FormatString ("<color=orange>{0}</color> left <color=blue>water</color>.", MyName));
+        CancelInvoke("RescueFromWater");
+        OnLeaveWater();
+        ThingConsole.Log(FormatString("<color=orange>{0}</color> left <color=blue>water</color>.", MyName));
     }
 
-    protected void SetTarget (Vector3 target) {
-        if (!stopWalkingAround) {
-            motor.SetTarget (target);
-            ThingConsole.Log (FormatString ("<color=orange>{0}</color> gained a <color=red>new</color> target.", MyName));
+    protected void SetTarget(Vector3 target)
+    {
+        if (!stopWalkingAround)
+        {
+            motor.SetTarget(target);
+            ThingConsole.Log(FormatString("<color=orange>{0}</color> gained a <color=red>new</color> target.", MyName));
         }
     }
 
-    protected void StopMoving () {
+    protected void StopMoving()
+    {
         stopWalkingAround = true;
-        motor.Stop ();
-        ThingConsole.Log (FormatString ("{0} stopped moving", MyName));
+        motor.Stop();
+        ThingConsole.Log(FormatString("{0} stopped moving", MyName));
     }
 
-    protected void StopMoving (float seconds) {
-        StopMoving ();
-        Invoke ("RestartWalking", seconds);
+    protected void StopMoving(float seconds)
+    {
+        StopMoving();
+        Invoke("RestartWalking", seconds);
     }
 
-    protected void Mute () {
+    protected void Mute()
+    {
         stopTalking = true;
-        ThingConsole.LogWarning (FormatString ("{0} is being muted.", MyName));
+        ThingConsole.LogWarning(FormatString("{0} is being muted.", MyName));
     }
 
-    protected void DeMute () {
+    protected void DeMute()
+    {
         stopTalking = false;
-        ThingConsole.Log (FormatString ("{0} can speak again", MyName));
+        ThingConsole.Log(FormatString("{0} can speak again", MyName));
     }
 
-    protected void RestartWalking () {
+    protected void RestartWalking()
+    {
         stopWalkingAround = false;
     }
 
-    protected void SetRandomTarget (float area) {
-        SetTarget (new Vector3 (Random.Range (-area, area), 0, Random.Range (-area, area)));
+    protected void SetRandomTarget(float area)
+    {
+        SetTarget(new Vector3(Random.Range(-area, area), 0, Random.Range(-area, area)));
     }
 
-    protected void AddForce (Vector3 f) {
-        motor.rb.AddForce (f);
+    protected void AddForce(Vector3 f)
+    {
+        motor.rb.AddForce(f);
     }
 
-    protected void SetScale (Vector3 newScale) {
+    protected void SetScale(Vector3 newScale)
+    {
         transform.localScale = newScale;
     }
 
-    protected void Speak (string content) {
+    protected void Speak(string content)
+    {
         // if (myChatBubble == null) return;
         // if (stopTalking) return;
         // if (speakCD.inCD) return;
@@ -266,97 +307,109 @@ public class Thing : MonoBehaviour {
         // speakCD.GoCooldown ();
     }
 
-    protected void Spark (Color particleColor, int numberOfParticles) {
+    protected void Spark(Color particleColor, int numberOfParticles)
+    {
 
-        if (explodePS == null) {
-            explodePS = GetComponentInChildren<ParticleSystem> ();
+        if (explodePS == null)
+        {
+            explodePS = GetComponentInChildren<ParticleSystem>();
             if (explodePS == null) return;
         }
 
         ParticleSystem.MainModule particleMain = explodePS.main;
         particleMain.startColor = particleColor;
-        var newBurst = new ParticleSystem.Burst (0f, numberOfParticles);
-        explodePS.emission.SetBurst (0, newBurst);
-        explodePS.Play ();
-        TTTEventsManager.main.SomeoneSparked (gameObject);
+        var newBurst = new ParticleSystem.Burst(0f, numberOfParticles);
+        explodePS.emission.SetBurst(0, newBurst);
+        explodePS.Play();
+        TTTEventsManager.main.SomeoneSparked(gameObject);
 
-        ThingConsole.Log (FormatString ("<color=orange>{0}</color> sparked", MyName));
+        ThingConsole.Log(FormatString("<color=orange>{0}</color> sparked", MyName));
     }
 
-    protected void CreateCube () {
-        GameObject acube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+    protected void CreateCube()
+    {
+        GameObject acube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         acube.layer = 12;
 
         acube.transform.localScale = Vector3.one / 4;
         acube.transform.position = transform.position;
 
-        if (generatedCubeContainer == null) {
-            generatedCubeContainer = new GameObject ();
+        if (generatedCubeContainer == null)
+        {
+            generatedCubeContainer = new GameObject();
             generatedCubeContainer.name = "Generated Cube Container";
-            generatedCubeContainer.AddComponent<ChildrenCounter> ();
+            generatedCubeContainer.AddComponent<ChildrenCounter>();
         }
 
         acube.transform.parent = generatedCubeContainer.transform;
-        generatedCubeContainer.GetComponent<ChildrenCounter> ().list.Add (acube);
+        generatedCubeContainer.GetComponent<ChildrenCounter>().list.Add(acube);
 
-        acube.AddComponent<Rigidbody> ();
-        acube.AddComponent<ProducedCube> ().Init (settings.myCubeColor);
+        acube.AddComponent<Rigidbody>();
+        acube.AddComponent<ProducedCube>().Init(settings.myCubeColor);
 
     }
 
-    protected void ResetColor () {
+    protected void ResetColor()
+    {
         if (mMat == null) return;
         mMat.color = originalColor;
-        ThingConsole.Log (FormatString ("{0} reset its own color", MyName));
+        ThingConsole.Log(FormatString("{0} reset its own color", MyName));
     }
 
-    protected void ChangeColor (Color c) {
+    protected void ChangeColor(Color c)
+    {
         if (mMat == null) return;
-        mMat.SetColor (matColor, c);
+        mMat.SetColor(matColor, c);
     }
 
-    protected void PlaySound (int soundFileID) {
-        if (soundFileID < 1 || soundFileID > 102) Debug.LogWarning ("sound file id exceed the range, range is 1->102, you are calling " + soundFileID);
-        PlaySound (soundFileID.ToString ());
+    protected void PlaySound(int soundFileID)
+    {
+        if (soundFileID < 1 || soundFileID > 102) Debug.LogWarning("sound file id exceed the range, range is 1->102, you are calling " + soundFileID);
+        PlaySound(soundFileID.ToString());
     }
 
-    protected void PlaySound (string soundFileName) {
+    protected void PlaySound(string soundFileName)
+    {
         if (playSoundCD.inCD) return;
-        playSoundCD.GoCooldown ();
-        audioSource.pitch = Random.Range (1.0f, 2.2f);
+        playSoundCD.GoCooldown();
+        audioSource.pitch = Random.Range(1.0f, 2.2f);
         if (audioSource.isPlaying) return;
         string soundUrl = soundFilePath + soundFileName;
-        audioSource.clip = Resources.Load (soundUrl) as AudioClip;
-        if (audioSource.clip != null) {
-            audioSource.Play ();
-            Spark (Color.white, 5);
+        audioSource.clip = Resources.Load(soundUrl) as AudioClip;
+        if (audioSource.clip != null)
+        {
+            audioSource.Play();
+            Spark(Color.white, 5);
         }
     }
 
-    protected ThingMotor GetMotor () {
+    protected ThingMotor GetMotor()
+    {
         return motor;
     }
 
-    protected void RandomSetDestination () {
-        SetRandomTarget (settings.newDestinationRange);
+    protected void RandomSetDestination()
+    {
+        SetRandomTarget(settings.newDestinationRange);
     }
 
-    protected void ResetPosition () {
-        motor.rb.position = ThingManager.main.GetSpawnPosition ();
+    protected void ResetPosition()
+    {
+        motor.rb.position = ThingManager.main.GetSpawnPosition();
         motor.rb.velocity = Vector3.zero;
     }
 
     //VIRTUAL
-    protected virtual void TTTAwake () { }
-    protected virtual void TTTStart () { }
-    protected virtual void TTTUpdate () { }
-    protected virtual void OnMeetingSomeone (GameObject other) { }
-    protected virtual void OnLeavingSomeone (GameObject other) { }
-    protected virtual void OnNeighborSpeaking () { }
-    protected virtual void OnNeigborSparkingParticles () { }
-    protected virtual void OnTouchWater () { }
-    protected virtual void OnLeaveWater () { }
-    protected virtual void OnSunset () { }
-    protected virtual void OnSunrise () { }
+    protected virtual void TTTAwake() { }
+    protected virtual void TTTStart() { }
+    protected virtual void TTTUpdate() { }
+    protected virtual void OnMeetingSomeone(GameObject other) { }
+    protected virtual void OnLeavingSomeone(GameObject other) { }
+    protected virtual void OnNeighborSpeaking() { }
+    protected virtual void OnNeigborSparkingParticles() { }
+    protected virtual void OnTouchWater() { }
+    protected virtual void OnLeaveWater() { }
+    protected virtual void OnSunset() { }
+    protected virtual void OnSunrise() { }
 
 }
